4688704bee3c1f129957e0b1d8e6b2f36a76f4a8
zav <zav.shotan@gmail.com>
2024-02-01 16:41:34 -0500

    updated

--- testing/test_aquisition.py
+++ testing/test_aquisition.py
@@ -1,9 +1,10 @@
 # %% Load config
 from tabor_client import TaborClient
 import time
+import re
 
 host = "134.74.27.64"
-#host = "134.74.27.62"
+# host = "134.74.27.62"
 port = "5025"
 client = TaborClient(host, port, keep_command_and_query_record=True)
 
@@ -12,38 +13,51 @@ client.connect()
 
 # %% Setup digitizer
 
+# client.command(
+#     ":DIG:CHAN CH1",
+#     ":DIG:INIT OFF",
+#     ":DIG:TRIG:SOUR CH1",
+#     ":DIG:CHAN:STAT ENAB",
+#     ":DIG:TRIG:TYPE EDGE",
+#     ":DIG:PULS INT,FIX,2e-8",
+#     ":DIG:INIT ON",
+# )
+
+# %%
+# Create counter
 client.command(
-    ":DIG:CHAN CH1",
-    ":DIG:INIT OFF",
-    ":DIG:TRIG:SOUR CH1",
-    ":DIG:CHAN:STAT ENAB",
-    ":DIG:TRIG:TYPE EDGE",
-    ":DIG:PULS INT,FIX,2e-8",
-    ":DIG:INIT ON",
+    ":DIG:CHAN CH2",
+    ":DIG: TRIG: SOUR CH2",
+    ":DIG: CHAN: STAT ENAB",
+    ":DIG: TRIG: TYPE EDGE",
+    ":DIG: INIT OFF",
+    ":DIG: PULS INT, FIX, 1e9",
+    ":DIG: INIT ON",
+    ":DIG:PULS: TRIG: IMM",
 )
 
-# %% Commands from mark
-client.command(':DIG:CHAN CH2',
-':DIG: TRIG: SOUR CH2',
-':DIG: CHAN: STAT ENAB',
-':DIG: TRIG: TYPE EDGE',
-':DIG: INIT OFF',
-':DIG: PULS INT, FIX, 1e9',
-':DIG: INIT ON',
-':DIG:PULS: TRIG: IMM',
-)
-for i in range(20):
-    time.sleep(20e-3)
-    print(client.query(':DIG: PULS: COUN?'))
-# %% Marks command
-time.sleep(2e-3)
-print(client.query(':DIG: PULS: COUN?'))
-# %% Query
 client.query(
     ":DIG:ACQ:STAT?",
     ":DIG:CHAN:STAT?",
     ":DIG:PULS:COUN?",
 )
+
+
+# %%
+# Test counter
+
+for i in range(20):
+    rslt: str = client.query(":DIG: PULS: COUN?")
+    if "," in rslt:
+        counts = int(re.split(r"[\s,]+", rslt.strip()))
+        print(counts)
+    else:
+        print("Error reading counts")
+    time.sleep(20e-3)
+
+# %% Marks command
+# time.sleep(2e-3)
+# print(client.query(":DIG: PULS: COUN?"))
 
 # %% Disconnect
 client.disconnect()
